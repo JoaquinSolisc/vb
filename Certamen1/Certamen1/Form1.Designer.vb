1fdc589dd2989e55d373ddb9828dea21e38666b8
Joaquin <joaquin.solisc@sansano.usm.cl>
2022-12-09 07:41:02 -0300

    Add files via upload

---
+++ Certamen1/Certamen1/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label251 = New System.Windows.Forms.Label()
+        Me.Label252 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label251
+        '
+        Me.Label251.AutoSize = True
+        Me.Label251.Location = New System.Drawing.Point(0, -1)
+        Me.Label251.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label251.Name = "Label251"
+        Me.Label251.Size = New System.Drawing.Size(102, 18)
+        Me.Label251.TabIndex = 0
+        Me.Label251.Text = "Nuevo juego"
+        '
+        'Label252
+        '
+        Me.Label252.AutoSize = True
+        Me.Label252.Location = New System.Drawing.Point(124, -1)
+        Me.Label252.Name = "Label252"
+        Me.Label252.Size = New System.Drawing.Size(81, 18)
+        Me.Label252.TabIndex = 1
+        Me.Label252.Text = "ESC Salir"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 200
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1269, 750)
+        Me.Controls.Add(Me.Label252)
+        Me.Controls.Add(Me.Label251)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.Name = "Form1"
+        Me.Text = "Joaquin Solis"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label251 As Label
+    Friend WithEvents Label252 As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Certamen1/Certamen1/Form1.vb
@@ -0,0 +1,136 @@
+﻿Public Class Form1
+    Dim lab As Label()
+    Dim posY, posX As Integer
+    Dim a As Integer
+    Dim u, d, t, c As Integer
+    Dim u2, d2, t2, c2 As Integer
+    Dim k As Integer
+    Dim cd As Integer
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label252.Click
+        End
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label251.Click
+        If k = 1 Then
+            lab(u2).BackColor = Color.White
+            lab(d2).BackColor = Color.White
+            lab(t2).BackColor = Color.White
+            lab(c2).BackColor = Color.White
+        End If
+        u = 5
+        d = 15
+        t = 25
+        c = 35
+        If k = 1 Then
+            lab(u).BackColor = Color.Red
+            lab(d).BackColor = Color.Red
+            lab(t).BackColor = Color.Red
+            lab(c).BackColor = Color.Red
+        End If
+        u2 = u
+        d2 = d
+        t2 = t
+        c2 = c
+        cd = 5
+        If k = 0 Then
+            crear_label()
+
+        End If
+
+        k = 1
+
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub crear_label()
+        a = 0
+        posY = 100
+        posX = 420
+        lab = New Label(250) {}
+
+        For i = 1 To 25
+            posY += 20
+            posX = 420
+            For j = 1 To 10
+                a = a + 1
+                lab(a) = New Label()
+                lab(a).AutoSize = False
+                lab(a).Size = New Size(20, 20)
+                lab(a).Location = New Point(posX, posY)
+                lab(a).BorderStyle = BorderStyle.FixedSingle
+                If a = 5 Or a = 15 Or a = 25 Or a = 35 Then
+                    lab(a).BackColor = Color.Red
+                Else
+                    lab(a).BackColor = Color.White
+                End If
+                Me.Controls.Add(lab(a))
+                posX += 20
+            Next
+        Next
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        lab(u2).BackColor = Color.White
+        lab(d2).BackColor = Color.White
+        lab(t2).BackColor = Color.White
+        lab(c2).BackColor = Color.White
+        u += 10
+        d += 10
+        t += 10
+        c += 10
+        u2 = u
+        d2 = d
+        t2 = t
+        c2 = c
+
+        lab(u).BackColor = Color.Red
+        lab(d).BackColor = Color.Red
+        lab(t).BackColor = Color.Red
+        lab(c).BackColor = Color.Red
+        If c > 240 Then
+            Timer1.Stop()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        u = 5
+        d = 15
+        t = 25
+        c = 35
+        k = 0
+        cd = 5
+
+    End Sub
+
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case 37
+                If cd > 1 Then
+                    u -= 1
+                    d -= 1
+                    t -= 1
+                    c -= 1
+                    cd = cd - 1
+                End If
+            Case 39
+                If cd < 10 Then
+                    u += 1
+                    d += 1
+                    t += 1
+                    c += 1
+                    cd = cd + 1
+                End If
+            Case Keys.Escape
+                End
+
+        End Select
+
+    End Sub
+End Class
